4a0ecd4e425c7e37c595881395bca2a492ba7a2f
unknown <mdabdulbari4646@gmail.com>
2022-07-02 07:51:31 +0600

    null vs undefined

---
+++ null-undefine.js
@@ -0,0 +1,19 @@
+let sojib;
+
+function add(num1, num2){
+    console.log(num1 + num2);
+    return
+}
+
+function add(num1, num2){
+    console.log(num1, num2);
+}
+
+const raju = {name: "smart boy", phone:12345};
+console.log(raju.gf);
+
+let funny = undefined;
+console.log(funny);
+
+let ages = [4, 5, 6, 7]
+console.log(ages[23]);
