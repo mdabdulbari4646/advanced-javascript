2ae3e541082990cc1164b512d553168ee871f701
unknown <mdabdulbari4646@gmail.com>
2022-07-02 09:35:52 +0600

    Scope

---
+++ scope.js
@@ -0,0 +1,21 @@
+let bonus = 20;
+
+function sum(first, second){
+    let result = first + second + bonus;
+    //console.log(bonus);
+    if(result > 10){
+        const mood = "off";
+        mood = "on";
+        mood = "kisuna"
+        // console.log(mood);
+    }
+    return result;
+}
+
+console.log(day);
+var day = "friday";
+return result;
+
+const output = sum(3, 5);
+console.log(bonus);
+console.log(output)
